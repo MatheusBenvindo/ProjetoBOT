5eceb3289c9b32ec178391faa0dc94d29d0e8b2b
MatheusBenvindo <mattbenvindo@gmail.com>
2024-11-18 08:32:39 -0300

    att
    att

--- pyautopoo.py
+++ pyautopoo.py
@@ -1,36 +1,28 @@
 import os
 import pyautogui
 
-# Classe para automação de tarefas com pyautogui
 class AutomacaoPyAutoGui:
     def __init__(self):
-        # Limpar a tela do terminal (em sistemas Windows)
         os.system('cls')
 
     def ativar_visualizacao_janelas(self):
-        # Ativa a visualização de janelas no Windows (Win + Tab)
         pyautogui.hotkey('win', 'tab')
         pyautogui.sleep(1)
     
     def clicar_imagem(self, img_path, confidence=0.6):
         pyautogui.sleep(1)
         
-        # Localiza o centro da imagem na tela
         img = pyautogui.locateCenterOnScreen(img_path, confidence=confidence)
         pyautogui.sleep(1)
         
-        # Se a imagem foi encontrada, clica nela
         if img is not None:
             pyautogui.sleep(1)
             pyautogui.click(img.x, img.y)
         else:
             print(f"Imagem {img_path} não encontrada na tela.")
             
-# Criando uma instância da classe AutomacaoPyAutoGui
 automacao = AutomacaoPyAutoGui()
 
-# Ativar a visualização de janelas
 automacao.ativar_visualizacao_janelas()
 
-# Clicar na imagem 'teste.png'
 automacao.clicar_imagem('teste.png', confidence=0.6)
--- pyautoprogestrut.py
+++ pyautoprogestrut.py
@@ -1,29 +1,32 @@
 import os
 import pyautogui
+import time
 
-# Limpar a tela do terminal (em sistemas Windows)
 os.system('cls')
 
-# Função para clicar em uma imagem na tela
 def clicar_imagem(img_path, confidence=0.6):
     pyautogui.sleep(1)
     
-    # Localiza o centro da imagem na tela
     img = pyautogui.locateCenterOnScreen(img_path, confidence=confidence)
     pyautogui.sleep(1)
     
-    # Se a imagem foi encontrada, clica nela
     if img is not None:
         pyautogui.sleep(1)
         pyautogui.click(img.x, img.y)
     else:
         print(f"Imagem {img_path} não encontrada na tela.")
 
-# Função para acionar a visualização de múltiplas janelas no Windows (Win + Tab)
 def ativar_visualizacao_janelas():
     pyautogui.hotkey('win', 'tab')
     pyautogui.sleep(1)
 
-# Ativar a visualização de janelas
+# Horário de 12:00 - Arena Guardião 
+# Abre o visualizador de tarefas e entra onde o jogo está rodando
 ativar_visualizacao_janelas()
 clicar_imagem ('teste.png', confidence=0.6)
+
+#localiza e clica no icone da batalha
+clicar_imagem ('teste1.png', confidence=0.6)
+
+#fecha ao terminar use biblioteca time
+
